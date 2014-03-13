d472fded48f396272ccdf85812ea8bae61c4e9b6
Tom Allen <tom@jugglethis.net>
2014-03-13 21:10:02 +1100

    Fixed broken tests having altered RoadPiece constructor args.

--- tests/test_RoadPiece.py
+++ tests/test_RoadPiece.py
@@ -27,46 +27,46 @@ class Test_RoadPiece(unittest.TestCase):
 
     def test_reverse_exits(self):
         # Cell bounds are the half-interval [0,cellSize) in each dimension.
-        self.assertEqual( ["E","N","S","W"], RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), 0, "NEWS").reverse_exit_list() )
-        self.assertEqual( ["E","N"], RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), 0, "WS").reverse_exit_list() )
-        self.assertEqual( [], RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), 0, "").reverse_exit_list() )
+        self.assertEqual( ["E","N","S","W"], RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), "NEWS").reverse_exit_list() )
+        self.assertEqual( ["E","N"], RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), "WS").reverse_exit_list() )
+        self.assertEqual( [], RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), "").reverse_exit_list() )
 
     def test_can_pieces_mate(self):
-        self.assertTrue( RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), 0, "NEWS").can_pieces_mate(RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), 0, "NEWS")) )
-        self.assertTrue( RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), 0, "N").can_pieces_mate(RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), 0, "S")) )
-        self.assertFalse( RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), 0, "").can_pieces_mate(RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), 0, "NEWS")) )
-        self.assertFalse( RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), 0, "NS").can_pieces_mate(RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), 0, "EW")) )
+        self.assertTrue( RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), "NEWS").can_pieces_mate(RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), "NEWS")) )
+        self.assertTrue( RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), "N").can_pieces_mate(RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), "S")) )
+        self.assertFalse( RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), "").can_pieces_mate(RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), "NEWS")) )
+        self.assertFalse( RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), "NS").can_pieces_mate(RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), "EW")) )
 
     def test_rotate_to_face_direction(self):
-        test_piece = RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), 0, "N")
+        test_piece = RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), "N")
         test_piece.rotate_to_face_direction("N") # Turning by zero degrees does nothing to exit_list.
         self.assertEqual( ["N"], test_piece.exit_list )
 
-        test_piece = RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), 0, "N")
+        test_piece = RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), "N")
         test_piece.rotate_to_face_direction("E") # Turning a piece with a North exit to face East, should yield an East exit.
         self.assertEqual( ["E"], test_piece.exit_list )
 
-        test_piece = RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), 0, "NEW")
+        test_piece = RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), "NEW")
         test_piece.rotate_to_face_direction("E") # Turning a piece with NEW exits to face East, should yield an ESN exits.
         self.assertEqual( ["E","N","S"], test_piece.exit_list )
 
-        test_piece = RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), 0, "N")
+        test_piece = RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), "N")
         test_piece.rotate_to_face_direction("S") # Turning a piece with a North exit to face South, should yield a South exit.
         self.assertEqual( ["S"], test_piece.exit_list )
 
-        test_piece = RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), 0, "NEW")
+        test_piece = RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), "NEW")
         test_piece.rotate_to_face_direction("S") # Turning a piece with NEW exits to face South, should yield SWE exits.
         self.assertEqual( ["E","S","W"], test_piece.exit_list )
 
-        test_piece = RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), 0, "N")
+        test_piece = RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), "N")
         test_piece.rotate_to_face_direction("W") # Turning a piece with a North exit to face West, should yield a West exit.
         self.assertEqual( ["W"], test_piece.exit_list )
 
-        test_piece = RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), 0, "NEW")
+        test_piece = RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), "NEW")
         test_piece.rotate_to_face_direction("W") # Turning a piece with NEW exits to face West, should yield WNS exits.
         self.assertEqual( ["N","S","W"], test_piece.exit_list )
 
-        test_piece = RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), 0, "N")
+        test_piece = RoadPiece.RoadPiece( self.the_game, os.path.join("tests","test_piece.png"), (0,0), "N")
         test_piece.rotate_to_face_direction("S") # Turning a piece with a North exit to face South, should yield a South exits.
         self.assertEqual( ["S"], test_piece.exit_list )
         test_piece.rotate_to_face_direction("S") # Turning it again to face South, should yield a North exit.
